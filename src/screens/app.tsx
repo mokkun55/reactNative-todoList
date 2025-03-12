9ea6ef4d68770d1c4405063e74d524b69866c8b9
もっくん <69388877+mokkun55@users.noreply.github.com>
2025-03-12 15:32:49 +0900

    Initial commit

---
+++ src/screens/app.tsx
@@ -0,0 +1,25 @@
+import { type FC } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { registerRootComponent } from "expo";
+import { StatusBar } from "expo-status-bar";
+
+const App: FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up app.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+// eslint-disable-next-line no-unused-expressions
+registerRootComponent(App);
