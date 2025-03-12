ce33a21bcb0b1a069481e90c27d1e0c42974e84a
もっくん <mokkunpc@gmail.com>
2025-03-12 23:01:39 +0900

    fix: homeページのmargin調整

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -70,7 +70,7 @@ export const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 48,
+    marginTop: 24,
     paddingHorizontal: 24,
   },
   title: {
